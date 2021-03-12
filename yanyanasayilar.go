fd4f293a997ae5c721d7ab2bdb927ba054b43b5f
eispiroglu <48657858+eispiroglu@users.noreply.github.com>
2021-03-12 15:38:55 +0300

    Update yanyanasayilar.go

--- yanyanasayilar.go
+++ yanyanasayilar.go
@@ -6,21 +6,21 @@ func main() {
 	num3 := []int{}
 	var a1, a2, a3, count, max int
 
-	num5 := digit5()
+	num5 := digit5() //Creating the array of all 5 digit numbers
 
-	for i, v := range num5 {
+	for i, v := range num5 { //Main loop that determinator the starting point of tries
 
 		count++
 
-		if i == 0 {
+		if i == 0 { //Creating the first unuseable numbers
 			a1 = f3(v)
 			a2 = s3(v)
 			a3 = t3(v)
 			num3 = append(num3, digit3(a1, a2, a3)...)
 		}
 
-		for _, x := range num5 {
-			if check(x, num3) == true {
+		for _, x := range num5 { //Second loop that compares the numbers are usuable or not
+			if check(x, num3) == true { //Updating the unuseable numbers
 				a1 = f3(x)
 				a2 = s3(x)
 				a3 = t3(x)
@@ -72,7 +72,7 @@ func digit5() []int {
 	}
 
 	return nums
-}
+} //Creates all 5 digit numbers and adds to an array
 
 func digit3(a1 int, a2 int, a3 int) []int {
 	num3 := []int{}
@@ -80,19 +80,19 @@ func digit3(a1 int, a2 int, a3 int) []int {
 	num3 = append(num3, a1, a2, a3)
 
 	return num3
-}
+} //Creates and updates unusable 3 digit numbers and adds to an array
 
 func f3(v int) int {
 	return (((v % 100000) / 10000) * 100) + ((v%10000)/1000)*10 + (v%1000)/100
-}
+} // Creates the unusable 3 digit numbers from the given number's first 3 digits
 
 func s3(v int) int {
 	return (((v % 10000) / 1000) * 100) + ((v%1000)/100)*10 + (v%100)/10
-}
+} // Creates the unusable 3 digit numbers from the given number's second 3 digits
 
 func t3(v int) int {
 	return (((v % 1000) / 100) * 100) + ((v%100)/10)*10 + (v%10)/1
-}
+} // Creates the unusable 3 digit numbers from the given number's third 3 digits
 
 func check(v int, num3 []int) bool {
 	a1 := f3(v)
@@ -105,4 +105,4 @@ func check(v int, num3 []int) bool {
 		}
 	}
 	return true
-}
+} //Checks the number is usable or not and return a boolean
